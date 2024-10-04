f1dbb89263dd15b0d50de8ca4d0eea1e17603701
Akhil T P <33170852+akhilelamadu@users.noreply.github.com>
2024-10-04 09:43:46 +0530

    Update ColorCoderTest.cpp

--- ColorCoderTest.cpp
+++ ColorCoderTest.cpp
@@ -20,12 +20,3 @@ void testPairToNumber(
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
-
-int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
-    return 0;
-}
